b961871cc8eb9918c58228a22303f9ae5b82433e
ItsTheIntrovertedGuy <max0018@t-online.de>
2020-03-21 19:53:23 +0100

    Updated routine if we already have a subject number, needs testing

--- app/src/main/java/com/example/selbsttest_corona/MainActivity.java
+++ app/src/main/java/com/example/selbsttest_corona/MainActivity.java
@@ -21,6 +21,8 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    CompositeDisposable compositeDisposable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,12 +34,11 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.testButton);
         button.setOnClickListener(new TestClass());
 
+
         // pruefe, ob subjectId bereits erstellt wurde auf diesem Geraet
-        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.selbsttest_corona", MODE_PRIVATE);
-        long subjectId = sharedPreferences.getLong("com.example.selbsttest_corona.subjectId", 0);
+        long subjectId = getSubjectIdFromSharedPreferences();
         if (subjectId == 0) {
-            // Subjekt id existiert nicht,
-            // TODO: Postleihzahl usw. abfragen
+            // Subjekt id existiert nicht, mit Programm fortfahren wie geplant
         } else {
             // Subjekt Id existiert
             Communication.getInstance().setSubjectId(subjectId);
@@ -46,11 +47,54 @@ public class MainActivity extends AppCompatActivity {
             //  - Neuen test
             //  - Generelle Informationen?
             //  - ...
+            //
+            // Aktuelle Alternative:
+            // Wir springen direkt zu der Frage
+            doSelftest();
         }
     }
 
+    public long getSubjectIdFromSharedPreferences() {
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.selbsttest_corona", MODE_PRIVATE);
+        long subjectId = sharedPreferences.getLong("com.example.selbsttest_corona.subjectId", 0);
+        return subjectId;
+    }
+
+    public void saveSubjectIdToSharedPreferences(long subjectId) {
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.selbsttest_corona", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putLong("com.example.selbsttest_corona.subjectId", subjectId);
+        editor.apply();
+    }
+
+    public void doSelftest() {
+        Single<Long> selftestIdJob = Communication.getInstance().createNewSelftest();
+        selftestIdJob.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        compositeDisposable.add(d);
+                    }
+
+                    @Override
+                    public void onSuccess(Long id) {
+                        Communication.getInstance().setSelftestId(id.longValue());
+                        Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                        compositeDisposable.dispose();
+                        startActivity(i);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast info = Toast.makeText(getApplicationContext(), "Fehler bei createNewSelftest", Toast.LENGTH_SHORT);
+                        compositeDisposable.dispose();
+                        info.show();
+                    }
+                });
+    }
+
     public class TestClass implements View.OnClickListener {
-        CompositeDisposable compositeDisposable;
 
         @Override
         public void onClick(View v) {
@@ -67,13 +111,17 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public void onSuccess(Long id) {
+                            // Speichern der Value fuer unseren uebergreifenden Communication Singleton,
+                            // ausserdem abspeichern in SharedPreferences, damit wir nicht
+                            // beim naechsten Start eine neue ID anmelden
                             Communication.getInstance().setSubjectId(id.longValue());
+                            saveSubjectIdToSharedPreferences(id.longValue());
                             doSelftest();
                         }
 
                         @Override
                         public void onError(Throwable e) {
-                            Toast info = Toast.makeText(getApplicationContext(), "Fehler", Toast.LENGTH_SHORT);
+                            Toast info = Toast.makeText(getApplicationContext(), "Fehler bei createSubjectId", Toast.LENGTH_SHORT);
                             compositeDisposable.dispose();
                             info.show();
                         }
@@ -84,33 +132,6 @@ public class MainActivity extends AppCompatActivity {
             textView.setText(postalNumber.getEditableText().toString());
             Button button = findViewById(R.id.testButton);
             button.setEnabled(false);
-        }
-
-        public void doSelftest() {
-            Single<Long> selftestIdJob = Communication.getInstance().createNewSelftest();
-            selftestIdJob.subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(new SingleObserver<Long>() {
-                        @Override
-                        public void onSubscribe(Disposable d) {
-                            compositeDisposable.add(d);
-                        }
-
-                        @Override
-                        public void onSuccess(Long id) {
-                            Communication.getInstance().setSelftestId(id.longValue());
-                            Intent i = new Intent(MainActivity.this, SecondActivity.class);
-                            compositeDisposable.dispose();
-                            startActivity(i);
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-                            Toast info = Toast.makeText(getApplicationContext(), "Fehler", Toast.LENGTH_SHORT);
-                            compositeDisposable.dispose();
-                            info.show();
-                        }
-                    });
         }
     }
 }
