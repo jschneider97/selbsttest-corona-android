f5531702016733c6a4cd564fa9dcef73dada1a0a
ItsTheIntrovertedGuy <max0018@t-online.de>
2020-03-21 17:29:03 +0100

    Added ability to load already created subjectId

--- app/src/main/java/com/example/selbsttest_corona/MainActivity.java
+++ app/src/main/java/com/example/selbsttest_corona/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.selbsttest_corona;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,12 +27,21 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.testButton);
         button.setOnClickListener(new TestClass());
 
-        // API fuer REST fuer die Interaktion mit dem server
-//        retrofit = new Retrofit.Builder()
-//                .baseUrl("TODO: API's base URL")
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//                .build();
+        // pruefe, ob subject bereits erstellt wurde auf diesem Geraet
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.selbsttest_corona", MODE_PRIVATE);
+        long subjectId = sharedPreferences.getLong("com.example.selbsttest_corona.subjectId", 0);
+        if (subjectId == 0) {
+            // Subjekt id existiert nicht,
+            // TODO: Postleihzahl usw. abfragen
+        } else {
+            // Subjekt Id existiert
+            Communication.getInstance().setSubjectId(subjectId);
+
+            // TODO: Zu einer Uebersicht springen
+            //  - Neuen test
+            //  - Generelle Informationen?
+            //  - ...
+        }
     }
 
     // Handler class fuer den Button
